be8e761de99de2d78a4cc36e229081cdc25bc66a
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-07-28 10:29:07 +0530

    Initial commit

---
+++ android/app/src/main/java/com/example/practice_project_flt/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.practice_project_flt;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/splash.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(MaterialApp(home: Insta()));
+}
+class Insta extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      body:
+      Center(
+        child:
+        ShaderMask(
+          blendMode: BlendMode.srcIn,
+          shaderCallback: (Rect bounds) => const LinearGradient(
+            begin: Alignment.topCenter,
+            end : Alignment.bottomLeft,
+            colors: [
+              Colors.purple,
+              Colors.pink,
+              Colors.redAccent,
+              Colors.orange,
+              Colors.yellowAccent,
+            ],
+          ).createShader(bounds),
+          child:
+          const FaIcon(FontAwesomeIcons.instagram,size: 100,),
+
+
+
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+// stops: [.5, 1,3],
+// colors: [
+// Colors.pink,
+// Colors.yellow,
+// Colors.blue,
+// ],
