b22e31a29f7e2c95ebf49bc66f702914ed9f6d38
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-08-01 13:54:39 +0530

    Initial Commit

--- lib/login.dart
+++ lib/login.dart
@@ -1,38 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_proj_1/home.dart';
 
-class LoginPage extends StatelessWidget{
+class LoginPage extends StatelessWidget {
+  String uname="admin";
+  String pass ="abc123";
+
+  TextEditingController usercontroller = TextEditingController();
+  TextEditingController passcontroller = TextEditingController();
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Login Page"),
+        title: const Text("Login Page"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Padding(
-                padding: EdgeInsets.all(20.0)),
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.people),
-                hintText: "UserName",
-                helperText: "User name must be an email",
-                labelText: "User Name",
+              padding: EdgeInsets.all(20.0),
+              child: TextField(
+                controller: usercontroller,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.people),
+                    hintText: "UserName",
+                    helperText: "user name must be an email",
+                    labelText: "UserName"),
               ),
             ),
-            TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  prefixIcon:Icon(Icons.visibility_off_sharp),
-                  hintText: "Password",
-                  helperText: "password must be a strong",
-                  labelText: "Password"),
+            const Padding(
+              padding: EdgeInsets.only(left: 20, right: 20,
+                  bottom: 50),
+              child: TextField(
+                controller: passcontroller,
+                obscureText: true,
+                obscuringCharacter: '*',
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.visibility_off_sharp),
+                    hintText: "Password",
+                    helperText: "password must contain 6 characters",
+                    labelText: "Password"),
+              ),
             ),
-
-            ElevatedButton(onPressed: (){}, child: Text("Login"))
+            ElevatedButton(onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) => HomePage()));
+            }, child: const Text("Login"))
           ],
         ),
       ),
